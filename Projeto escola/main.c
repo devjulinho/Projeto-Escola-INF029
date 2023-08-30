97f0c242c322e86585f23285e80429ceb5df78a7
juliobr1 <142620967+juliobr1@users.noreply.github.com>
2023-08-30 18:46:15 +0000

    Menu aluno (1 e 2)

--- Projeto escola/main.c
+++ Projeto escola/main.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 
+#define MAX_ALUNO 3
+#define MAX_PROFESSOR 3
+#define MAX_DISCIPLINA 3
+
+typedef struct aluno{
+  int matricula;
+  char nome;
+  char sexo;
+  char nascimento;
+  char CPF;
+} Aluno;
+
 int main(void){
+  int listaAluno[MAX_ALUNO];
+  int qtdAluno = 0;
   int opcao;
   int sair = 0;
 
+  // Menu inicial
   while (!sair){
   printf("Projeto Escola\n");
   printf("0- Sair\n");
@@ -12,20 +27,90 @@ int main(void){
   printf("3- Disciplina\n");
   
   scanf("%d", &opcao);
-  
+
+    // Switch do menu inicial
   switch(opcao){
-    case 0:{printf("Saindo do sistema\n"); sair = 1; break;};
-    case 1:{printf("Modulo - Aluno\n"); break;}
-    case 2:{printf("Modulo - Professor\n"); break;}
-    case 3:{printf("Modulo - Disciplina\n"); break;}
-    default:{printf("Opcao invalida\n");}}
+    case 0:{
+      printf("Saindo do sistema\n");
+      sair = 1;
+      break;}
+
+    // MENU ALUNO
+    case 1:{
+      printf("Modulo - Aluno\n");
+      int sairAluno = 0;
+      int opcaoAluno;
+      while(!sairAluno){
+        printf("0 - Voltar\n");
+        printf("1 - Cadastrar Aluno\n");
+        printf("2 - Listar Aluno\n");
+        printf("3 - Atualizar Aluno\n");
+        printf("4 - Excluir Aluno\n");
+        
+        scanf("%d", &opcaoAluno);
+
+        switch (opcaoAluno){
+          case 0:{
+            sairAluno = 1;
+            printf("Retornando ao menu inicial\n");
+            break;
+          }
+
+          case 1:{
+            printf("Cadastrar Aluno\n");
+            if (qtdAluno == MAX_ALUNO){
+              printf("Lista de alunos cheia");}
+            else{
+              printf("Digite a matricula\n");
+              int matricula;
+              scanf("%d", &matricula);
+              if (matricula < 0){
+                printf("Matrícula Inválida");}
+              else{
+                listaAluno[qtdAluno] = matricula;
+                qtdAluno++;
+                printf("Matrícula realizada com sucesso");
+              }
+            }
+          break;
+          }
+
+          case 2:{
+            printf("Listar Aluno\n");
+            if (qtdAluno == 0)
+              printf("Lista vazia\n");
+            else{
+              for (int i; i < qtdAluno; i++){
+                printf("Matrícula: %d\n", listaAluno[i]);
+              }}
+            break;
+          }
+
+          case 3:{
+            printf("Atualizar Aluno\n");
+            break;
+          }
+
+          case 4:{
+            printf("Excluir Aluno\n");
+            break;
+          }
+          
+        }
+      }
+      break;
+      }
     
+    case 2:{
+      printf("Modulo - Professor\n");
+      break;}
+    
+    case 3:{
+      printf("Modulo - Disciplina\n");
+      break;}
+    
+    default:{
+      printf("Opcao invalida\n");}
+    }
   }
-
-  
-
-  
-
-  
-  
 }
